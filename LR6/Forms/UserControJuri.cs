7befb2022e49eb3910d5f5cc5688e2c602ca28bc
Cheveux <Cheveux@DESKTOP-LVSGVTM>
2024-09-23 15:55:12 +0300

    Добавьте файлы проекта.

---
+++ LR6/Forms/UserControJuri.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LR6.DBCon; 
+
+namespace LR6.Forms
+{
+    public partial class UserControJuri : UserControl
+    {
+        public UserControJuri()
+        {
+            InitializeComponent();
+        }
+      
+        public void Fill(User user, int number )
+        {
+            label1.Text = "Жюри - " + number.ToString();
+            userBindingSource.DataSource = user;
+        }
+    }
+}
